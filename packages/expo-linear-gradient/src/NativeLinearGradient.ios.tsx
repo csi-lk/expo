18985a372ea021cbb8f96e8a6ef9e27e5ff813ac
Cedric van Putten <github@cedric.dev>
2024-07-23 18:49:27 +0200

    refactor: update `expo-modules-core` imports to `expo` or `expo/internal`

--- packages/expo-linear-gradient/src/NativeLinearGradient.ios.tsx
+++ packages/expo-linear-gradient/src/NativeLinearGradient.ios.tsx
@@ -1,4 +1,4 @@
-import { requireNativeViewManager } from 'expo-modules-core';
+import { requireNativeViewManager } from 'expo';
 import * as React from 'react';
 
 import { NativeLinearGradientProps } from './NativeLinearGradient.types';
